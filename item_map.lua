e11c6282f3d5104b2949e3d2c27ccbae34b94b8b
qff233 <347633553@qq.com>
2024-10-16 22:40:23 +0800

    fix: item_map

--- item_map.lua
+++ item_map.lua
@@ -1,6 +1,6 @@
 return {
-    ["360k NaK Coolantcell"] = "360k He Coolant Cell",
-    ["Master Magneto Resonatic Circuit"] = "Any Luv Circuit",
-    ["Elite Magneto Resonatic Circuit"] = "Any IV circuit",
-    ["Data Magneto Resonatic Circuit"] = "Any EV Circuit",
+	["360k NaK Coolantcell"] = "360k He Coolant Cell",
+	["Master Magneto Resonatic Circuit"] = "Any LuV Circuit",
+	["Elite Magneto Resonatic Circuit"] = "Any IV circuit",
+	["Data Magneto Resonatic Circuit"] = "Any EV Circuit",
 }
